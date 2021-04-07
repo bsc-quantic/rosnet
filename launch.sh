784b14c6cfb71a9df7b947284c4235e14e739a1c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-04-07 16:47:03 +0000

    Add node number option to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 PARAMS=""
+NUM_NODES=1
 EXEC_TIME=2
 while (( "$#" )); do
 	case "$1" in
 		-t|--exec_time)
 			EXEC_TIME=$2
+			shift 2
+			;;
+		-n|--num_nodes)
+			NUM_NODES=$2
 			shift 2
 			;;
 		-t=*|--exec_time=*)
@@ -20,4 +25,4 @@ while (( "$#" )); do
 done
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
-enqueue_compss --num_nodes=1 --qos=debug --log_level=info --exec_time=$EXEC_TIME --summary --graph $PARAMS
+enqueue_compss --num_nodes=$NUM_NODES --qos=debug --log_level=info --exec_time=$EXEC_TIME --summary --graph $PARAMS
