60f1a5e95ed9b857d8242bd2c0e5ac958d358fcb
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-04-07 14:24:13 +0000

    Fix PYTHONPATH on launch.sh
    $(dirname $0) was more convenient, but crashed

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PYTHONPATH=$(dirname $0):$PYTHONPATH
+export PYTHONPATH=$(pwd):$PYTHONPATH
 enqueue_compss --num_nodes=1 --qos=debug --log_level=info --exec_time=2 --summary --graph $@
