59fdf35752c0f7ecc8c77bbac64ec47363ed3707
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-04-07 16:40:38 +0000

    Parametrize launch.sh script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+
+PARAMS=""
+EXEC_TIME=2
+while (( "$#" )); do
+	case "$1" in
+		-t|--exec_time)
+			EXEC_TIME=$2
+			shift 2
+			;;
+		-t=*|--exec_time=*)
+			echo "Error: Unsupported ="
+			exit 1
+			;;
+		*)
+			PARAMS="$PARAMS $1"
+			shift 1
+			;;
+	esac
+done
+
 export PYTHONPATH=$(pwd):$PYTHONPATH
-enqueue_compss --num_nodes=1 --qos=debug --log_level=info --exec_time=2 --summary --graph $@
+enqueue_compss --num_nodes=1 --qos=debug --log_level=info --exec_time=$EXEC_TIME --summary --graph $PARAMS
