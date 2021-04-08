5e60e494df3eb5953de2de4bf35eb5500c8e84f2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-04-08 16:47:01 +0000

    Add block rank option for tests

--- test/test_tensor_init.py
+++ test/test_tensor_init.py
@@ -3,14 +3,16 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Tensor.transpose test')
+    parser = argparse.ArgumentParser(description='Tensor.__init__ test')
     parser.add_argument('n', metavar='n', type=int,
                         nargs=1, help='Tensor rank')
+    parser.add_argument('m', type=int, nargs=1, help='Block rank')
     args = parser.parse_args()
     n = args.n[0]
+    m = args.m[0]
 
-    shape = [4] * n
-    block_shape = [2] * (n//2) + [1] * (n//2)
+    shape = [2] * n
+    block_shape = [2] * (n-m) + [1] * (m)
     t = Tensor.zeros(shape, block_shape)
     t.sync()
     print(
--- test/test_tensor_transpose.py
+++ test/test_tensor_transpose.py
@@ -6,11 +6,13 @@ def main():
     parser = argparse.ArgumentParser(description='Tensor.transpose test')
     parser.add_argument('n', metavar='n', type=int,
                         nargs=1, help='Tensor rank')
+    parser.add_argument('m', type=int, nargs=1, help='Block rank')
     args = parser.parse_args()
     n = args.n[0]
+    m = args.m[0]
 
-    shape = [4] * n
-    block_shape = [2] * (n//2) + [1] * (n//2)
+    shape = [2] * n
+    block_shape = [2] * (n-m) + [1] * (m)
     t = Tensor.zeros(shape, block_shape)
     print(
         f'Created tensor with rank={t.rank}, shape={t.shape}, block_shape={t.block_shape}, grid={t.grid}')
