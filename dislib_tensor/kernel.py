eae1c813a7beda55a73f45c07cd60b92237361a2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-04-13 13:43:19 +0000

    Delete debug messages in kernel.block_merge_split

--- dislib_tensor/kernel.py
+++ dislib_tensor/kernel.py
@@ -12,34 +12,20 @@ def block_transpose(block: np.ndarray, permutator):
 
 @ task(blocks={Type: COLLECTION_INOUT, Depth: 1})
 def block_merge_split(blocks, block_shape, a):
-    print("[DEBUG] block_merge_split:")
-    print("\ta = %s" % str(a))
-    print("\tblock_shape = %s" % block_shape)
-    print("\tlen(blocks) = %s" % len(blocks))
-
     # merge blocks
-    print("\tmerge blocks:")
     superblock = np.block(blocks).reshape((-1, len(blocks)), order='F')
-    print("\t\t1 - shape(superblock) = %s" % str(superblock.shape))
     superblock = np.reshape(superblock, block_shape + [len(blocks)], order='F')
-    print("\t\t2 - shape(superblock) = %s" % str(superblock.shape))
 
     # transpose indexes
-    print("\ttranspose indexes:")
     permutator = list(range(len(block_shape) + 1))
     permutator[a], permutator[-1] = permutator[-1], permutator[a]
-    print("\t\tpermutator = %s" % str(permutator))
     superblock = np.transpose(superblock, permutator)
-    print("\t\tshape(superblock) = %s" % str(superblock.shape))
 
     # split blocks
-    print("\tsplit blocks:")
     blocks = np.split(
         superblock, superblock.shape[-1], axis=len(superblock.shape)-1)
     blocks = [np.reshape(block, (prod(block.shape),), order='F')
               for block in blocks]
-    print("\t\tlen(blocks) = %s" % str(len(blocks)))
-    print("\t\tblocks[0].shape = %s" % str(blocks[0].shape))
 
 
 @task(returns=np.array)
