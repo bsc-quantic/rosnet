a3d789627a762011b78f140036f53ad10127d46e
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-05-05 13:07:34 +0000

    Fix launch working directory
    COMPSs relies on serializing to network-mounted directories.

--- launch.sh
+++ launch.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-PARAMS=""
+# defaults
+PARAMS=()
 NUM_NODES=1
 EXEC_TIME=2
-LOG_LEVEL=info
+LOG_LEVEL=off
 TRACE=false
+USER=$(id -u -n)
+GROUP=$(id -g -n)
+WORKER_WD=/gpfs/scratch/$GROUP/$USER
+MASTER_WD=/gpfs/scratch/$GROUP/$USER
+
+# parse arguments
 while (( "$#" )); do
 	case "$1" in
 		-t|--exec_time)
@@ -23,16 +30,29 @@ while (( "$#" )); do
 			TRACE=$2
 			shift 2
 			;;
+		--worker_working_dir)
+			WORKER_WD=$2
+			shift 2
+			;;
+		--master_working_dir)
+			MASTER_WD=$2
+			shift 2
+			;;
 		-*=*|--*=*)
-			echo "Error: Unsupported ="
+			echo "Error: Unsupported '=' syntax"
 			exit 1
 			;;
 		*)
-			PARAMS="$PARAMS $1"
+			PARAMS+=($1)
 			shift 1
 			;;
 	esac
 done
 
+# fix execution from working dir
+if [[ "${PARAMS[0]}" != /* ]]; then
+	PARAMS[0]=$PWD/${PARAMS[0]}
+fi
+
 export PYTHONPATH=$(pwd):$PYTHONPATH
-enqueue_compss --num_nodes=$NUM_NODES --qos=debug --log_level=$LOG_LEVEL --exec_time=$EXEC_TIME --summary --graph --tracing=$TRACE $PARAMS
+enqueue_compss --num_nodes=$NUM_NODES --qos=debug --log_level=$LOG_LEVEL --exec_time=$EXEC_TIME --worker_working_dir=$WORKER_WD --master_working_dir=$MASTER_WD --summary --graph --tracing=$TRACE $PARAMS
