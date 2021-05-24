111f89da3d1d0882e809d42969db7c61bdbe6ca7
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-05-24 17:24:25 +0000

    Fix return statement in block_identity

--- rosnet/kernel.py
+++ rosnet/kernel.py
@@ -112,6 +112,8 @@ def block_identity(block_shape, n, i, j, dtype):
 
     block[i_ones, j_ones] = 1
 
+    return block
+
 
 @task(U={Type: COLLECTION_INOUT, Depth: 2}, V={Type: COLLECTION_INOUT, Depth: 2})
 def svdmatrix_async_blocked(U, V, eps: float):
