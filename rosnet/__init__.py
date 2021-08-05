4f4ea8c3ee1bd0b282164fa0c26d5818a83863b4
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-08-05 12:44:07 +0000

    Fix opt-einsum support

--- rosnet/__init__.py
+++ rosnet/__init__.py
@@ -1,2 +1,14 @@
 __version__ = "0.1"
 from rosnet.array import COMPSsArray, BlockArray, full, ones, zeros, rand, array
+
+# opt-einsum support
+from rosnet.array import __compss_tensordot, __block_tensordot
+
+
+def tensordot(a, b, axes):
+    if isinstance(a, COMPSsArray) and isinstance(b, COMPSsArray):
+        return __compss_tensordot(a, b, axes)
+    elif isinstance(a, BlockArray) and isinstance(b, BlockArray):
+        return __block_tensordot(a, b, axes)
+    else:
+        return NotImplemented
