1da13b1c91fc486f902380f794b71b37464c2bf5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-09-15 10:12:02 +0200

    Hardcode dynamic parallelism variants of tensordot tasks
    Dynamic parallelism is not yet supported in COMPSs. Only option is to hard-code variants.

--- rosnet/task/tensordot.py
+++ rosnet/task/tensordot.py
@@ -10,15 +10,125 @@ def sequential(a, b, axes):
     return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
 
 
+@constraint(computing_units="1")
+@task(
+    a={Type: COLLECTION_IN, Depth: 1},
+    b={Type: COLLECTION_IN, Depth: 1},
+    returns=np.ndarray,
+)
+def sequential_1(a, b, axes):
+    return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
+
+
+@constraint(computing_units="2")
+@task(
+    a={Type: COLLECTION_IN, Depth: 1},
+    b={Type: COLLECTION_IN, Depth: 1},
+    returns=np.ndarray,
+)
+def sequential_2(a, b, axes):
+    return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
+
+
+@constraint(computing_units="4")
+@task(
+    a={Type: COLLECTION_IN, Depth: 1},
+    b={Type: COLLECTION_IN, Depth: 1},
+    returns=np.ndarray,
+)
+def sequential_4(a, b, axes):
+    return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
+
+
+@constraint(computing_units="8")
+@task(
+    a={Type: COLLECTION_IN, Depth: 1},
+    b={Type: COLLECTION_IN, Depth: 1},
+    returns=np.ndarray,
+)
+def sequential_8(a, b, axes):
+    return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
+
+
+@constraint(computing_units="12")
+@task(
+    a={Type: COLLECTION_IN, Depth: 1},
+    b={Type: COLLECTION_IN, Depth: 1},
+    returns=np.ndarray,
+)
+def sequential_12(a, b, axes):
+    return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
+
+
 @constraint(computing_units="$NCORES", memory_size="$MEMORY")
 @task(ba=IN, bb=IN, returns=np.ndarray)
 def tensordot(ba, bb, axes):
     return np.tensordot(ba, bb, axes)
 
 
+@constraint(computing_units="1")
+@task(ba=IN, bb=IN, returns=np.ndarray)
+def tensordot_1(ba, bb, axes):
+    return np.tensordot(ba, bb, axes)
+
+
+@constraint(computing_units="2")
+@task(ba=IN, bb=IN, returns=np.ndarray)
+def tensordot_2(ba, bb, axes):
+    return np.tensordot(ba, bb, axes)
+
+
+@constraint(computing_units="4")
+@task(ba=IN, bb=IN, returns=np.ndarray)
+def tensordot_4(ba, bb, axes):
+    return np.tensordot(ba, bb, axes)
+
+
+@constraint(computing_units="8")
+@task(ba=IN, bb=IN, returns=np.ndarray)
+def tensordot_8(ba, bb, axes):
+    return np.tensordot(ba, bb, axes)
+
+
+@constraint(computing_units="12")
+@task(ba=IN, bb=IN, returns=np.ndarray)
+def tensordot_12(ba, bb, axes):
+    return np.tensordot(ba, bb, axes)
+
+
 @constraint(computing_units="$NCORES", memory_size="$MEMORY")
 @task(res=COMMUTATIVE)
 def commutative(res, a, b, axes):
+    res += np.tensordot(a, b, axes)
+
+
+@constraint(computing_units="1")
+@task(res=COMMUTATIVE)
+def commutative_1(res, a, b, axes):
+    res += np.tensordot(a, b, axes)
+
+
+@constraint(computing_units="2")
+@task(res=COMMUTATIVE)
+def commutative_2(res, a, b, axes):
+    res += np.tensordot(a, b, axes)
+
+
+@constraint(computing_units="4")
+@task(res=COMMUTATIVE)
+def commutative_4(res, a, b, axes):
+    res += np.tensordot(a, b, axes)
+
+
+@constraint(computing_units="8")
+@task(res=COMMUTATIVE)
+def commutative_8(res, a, b, axes):
+    res += np.tensordot(a, b, axes)
+
+
+@constraint(computing_units="12")
+@task(res=COMMUTATIVE)
+def commutative_12(res, a, b, axes):
     res += np.tensordot(a, b, axes)
 
 
