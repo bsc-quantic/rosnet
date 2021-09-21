9717f7141d3ad34e7f93fbb5e486b0b64d84ce03
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-09-21 21:36:06 +0200

    Refactor tuning.constraint from environment to global variables

--- rosnet/tuning/constraint.py
+++ rosnet/tuning/constraint.py
@@ -1,50 +1,28 @@
 from contextlib import contextmanager
-from os import environ
 
 # NOTE not thread-safe
-# NCORES = 1
-# MEMORY = None
-environ["NCORES"] = "1"
-environ["MEMORY"] = "0"
-
-
-# @contextmanager
-# def resources(**kwargs):
-#     # pylint: disable=global-statement
-#     # TODO COMPSs crashes with global variables
-#     global NCORES
-#     global MEMORY
-
-#     tmp = {
-#         "NCORES": NCORES,
-#         "MEMORY": MEMORY,
-#     }
-
-#     NCORES = kwargs.get("ncores") or tmp["NCORES"]
-#     MEMORY = kwargs.get("memory") or tmp["MEMORY"]
-
-#     yield
-
-#     NCORES = tmp["NCORES"]
-#     MEMORY = tmp["MEMORY"]
+NCORES = 1
+MEMORY = None
 
 
 @contextmanager
 def resources(**kwargs):
-    tmp = {
-        "NCORES": environ["NCORES"],
-        "MEMORY": environ["MEMORY"],
-    }
+    # pylint: disable=global-statement
+    global NCORES
+    global MEMORY
 
-    environ["NCORES"] = str(kwargs.get("ncores") or tmp["NCORES"])
-    environ["MEMORY"] = str(kwargs.get("memory") or tmp["MEMORY"])
+    tmp_ncores = NCORES
+    tmp_memory = MEMORY
 
     __fix_dyn_par_tensordot()
 
+    NCORES = kwargs.get("ncores") or tmp_ncores
+    MEMORY = kwargs.get("memory") or tmp_memory
+
     yield
 
-    environ["NCORES"] = tmp["NCORES"]
-    environ["MEMORY"] = tmp["MEMORY"]
+    NCORES = tmp_ncores
+    MEMORY = tmp_memory
 
     __fix_dyn_par_tensordot()
 
@@ -53,27 +31,27 @@ def resources(**kwargs):
 def __fix_dyn_par_tensordot():
     from rosnet.task import tensordot
 
-    if int(environ["NCORES"]) == 1:
+    if NCORES == 1:
         tensordot.sequential = tensordot.sequential_1
         tensordot.tensordot = tensordot.tensordot_1
         tensordot.commutative = tensordot.commutative_1
-    elif int(environ["NCORES"]) == 2:
+    elif NCORES == 2:
         tensordot.sequential = tensordot.sequential_2
         tensordot.tensordot = tensordot.tensordot_2
         tensordot.commutative = tensordot.commutative_2
-    elif 2 < int(environ["NCORES"]) <= 4:
+    elif 2 < NCORES <= 4:
         tensordot.sequential = tensordot.sequential_4
         tensordot.tensordot = tensordot.tensordot_4
         tensordot.commutative = tensordot.commutative_4
-    elif 4 < int(environ["NCORES"]) <= 8:
+    elif 4 < NCORES <= 8:
         tensordot.sequential = tensordot.sequential_8
         tensordot.tensordot = tensordot.tensordot_8
         tensordot.commutative = tensordot.commutative_8
-    elif 8 < int(environ["NCORES"]) <= 12:
+    elif 8 < NCORES <= 12:
         tensordot.sequential = tensordot.sequential_12
         tensordot.tensordot = tensordot.tensordot_12
         tensordot.commutative = tensordot.commutative_12
-    elif 12 < int(environ["NCORES"]) <= 24:
+    elif 12 < NCORES <= 24:
         tensordot.sequential = tensordot.sequential_24
         tensordot.tensordot = tensordot.tensordot_24
         tensordot.commutative = tensordot.commutative_24
