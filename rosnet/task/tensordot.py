5bc65c22b1de09bd1a4453b3e31440a54169de13
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-11-23 11:37:31 +0100

    Move tensordot dispatchers to task-definition file

--- rosnet/task/tensordot.py
+++ rosnet/task/tensordot.py
@@ -43,6 +43,23 @@ def _fix_blas_threads():
 #     return sum(np.tensordot(ba, bb, axes) for ba, bb in zip(a, b))
 
 
+def dispatch_sequential(a: List, b: List, axes):
+    # pylint: disable=protected-access
+    import rosnet
+
+    blockshape = result_shape(a[0].shape, b[0].shape, axes)
+    dtype = np.result_type(
+        *(i.dtype if hasattr(i, "dtype") else i for i in chain(a, b))
+    )
+
+    # get ref if a,b are COMPSsArrays
+    aref = [i._ref if hasattr(i, "_ref") else i for i in a]
+    bref = [i._ref if hasattr(i, "_ref") else i for i in b]
+
+    ref = rosnet.task.tensordot.sequential(aref, bref, axes)
+    return rosnet.COMPSsArray(ref, shape=blockshape, dtype=dtype)
+
+
 @constraint(computing_units="1")
 @task(
     a={Type: COLLECTION_IN, Depth: 1},
@@ -118,6 +135,26 @@ sequential = sequential_1
 #     return np.tensordot(ba, bb, axes)
 
 
+def dispatch_tensordot(a: List, b: List, axes):
+    # pylint: disable=protected-access
+    import rosnet
+
+    if len(a) > 1 or len(b) > 1:
+        raise ValueError()
+
+    blockshape = result_shape(a[0].shape, b[0].shape, axes)
+    dtype = np.result_type(
+        *(i.dtype if hasattr(i, "dtype") else i for i in chain(a, b))
+    )
+
+    # get ref if a,b are COMPSsArrays
+    aref = [i._ref if hasattr(i, "_ref") else i for i in a][0]
+    bref = [i._ref if hasattr(i, "_ref") else i for i in b][0]
+
+    ref = rosnet.task.tensordot.tensordot(aref, bref, axes)
+    return rosnet.COMPSsArray(ref, shape=blockshape, dtype=dtype)
+
+
 @constraint(computing_units="1")
 @task(ba=IN, bb=IN, returns=np.ndarray)
 def tensordot_1(ba, bb, axes):
@@ -167,6 +204,27 @@ tensordot = tensordot_1
 # @task(res=COMMUTATIVE)
 # def commutative(res, a, b, axes):
 #     res += np.tensordot(a, b, axes)
+
+
+def dispatch_commutative(a: List, b: List, axes):
+    # pylint: disable=protected-access
+    import rosnet
+
+    blockshape = result_shape(a[0].shape, b[0].shape, axes)
+    dtype = np.result_type(
+        *(i.dtype if hasattr(i, "dtype") else i for i in chain(a, b))
+    )
+
+    buffer = rosnet.task.tensordot.ArrayWrapper()
+
+    for ia, ib in zip(a, b):
+        # get ref if a,b are COMPSsArrays
+        ia = ia._ref if hasattr(ia, "_ref") else ia
+        ib = ib._ref if hasattr(ib, "_ref") else ib
+
+        rosnet.task.tensordot.commutative(buffer, ia, ib, axes)
+
+    return rosnet.COMPSsArray(buffer, shape=blockshape, dtype=dtype)
 
 
 @constraint(computing_units="1")
