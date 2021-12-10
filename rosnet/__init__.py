f78873c004932812fbd7aa649263275ff02f4b24
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2021-12-10 17:50:36 +0100

    Fix rosnet.tensordot

--- rosnet/__init__.py
+++ rosnet/__init__.py
@@ -1,14 +1,13 @@
 __version__ = "0.1"
 from rosnet.array import COMPSsArray, BlockArray, full, ones, zeros, rand, array
 
-# opt-einsum support
-from rosnet.array import __compss_tensordot, __block_tensordot
-
 
 def tensordot(a, b, axes):
+    import numpy as np
+
     if isinstance(a, COMPSsArray) and isinstance(b, COMPSsArray):
-        return __compss_tensordot(a, b, axes)
+        return np.tensordot(a, b, axes)
     elif isinstance(a, BlockArray) and isinstance(b, BlockArray):
-        return __block_tensordot(a, b, axes)
+        return np.tensordot(a, b, axes)
     else:
         return NotImplemented
