2d12d9f642e601505b8a8fc31f7c54342f4e5d5c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-02-09 18:21:51 +0100

    Rename BlockArray, COMPSsArray, MaybeArray filenames

--- rosnet/array/__init__.py
+++ rosnet/array/__init__.py
@@ -1,6 +1,6 @@
-from rosnet.array.blockarray import BlockArray
+from rosnet.array.block import BlockArray
 
 try:
-    from rosnet.array.compssarray import COMPSsArray
+    from rosnet.array.compss import COMPSsArray
 except ImportError:
     pass
