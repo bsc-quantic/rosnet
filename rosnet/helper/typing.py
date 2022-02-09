c86d7db70d2977bdbe47986e8b24421018aa7b37
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-02-09 17:43:58 +0100

    Fix typing protocols

--- rosnet/helper/typing.py
+++ rosnet/helper/typing.py
@@ -1,41 +1,25 @@
-from typing import Protocol, Tuple, List
+from typing import Protocol, Tuple, List, runtime_checkable
 import numpy as np
 from plum import parametric, type_of, ptype
 
 
+@runtime_checkable
 class SupportsArray(Protocol):
     def __array__(self) -> np.ndarray:
         pass
 
 
-class Array(SupportsArray):
+@runtime_checkable
+class Array(SupportsArray, Protocol):
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         pass
 
     def __array_function__(self, function, types, args, kwargs):
         pass
 
-    @property
     def shape(self) -> Tuple[int]:
         pass
 
-    @property
-    def size(self) -> int:
-        pass
-
-    @property
-    def itemsize(self) -> int:
-        pass
-
-    @property
-    def nbytes(self) -> int:
-        pass
-
-    @property
-    def ndim(self) -> int:
-        pass
-
-    @property
     def dtype(self) -> np.dtype:
         pass
 
