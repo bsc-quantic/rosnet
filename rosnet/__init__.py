d3cff1211c44be6087397e18a54dd9c08447ab8a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-02-14 12:42:41 +0100

    Export zeros, ones, full, rand array-creation methods
    Defaults to BlockArray methods

--- rosnet/__init__.py
+++ rosnet/__init__.py
@@ -1,15 +1,25 @@
 __version__ = "0.1"
+
 from rosnet.array import *
 from rosnet.numpy_interface import (
-	tensordot,
-	einsum,
-	reshape,
-	transpose,
-	stack,
-	split,
-	block,
-	zeros_like,
-	ones_like,
-	full_like,
-	empty_like
+    tensordot,
+    einsum,
+    reshape,
+    transpose,
+    stack,
+    split,
+    block,
+    zeros_like,
+    ones_like,
+    full_like,
+    empty_like,
+)
+
+# NumPy methods whose output type cannot be inferred. defaults to BlockArray.
+# for other kinds of arrays, use `autoray.do(..., like="rosnet.CLASSNAME")`
+from rosnet.array.block import (
+    zeros,
+    ones,
+    full,
+    rand,
 )
