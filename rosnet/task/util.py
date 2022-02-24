d786e2746dd9f1e0d942104cb76e502e0ef104d1
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-02-25 00:57:33 +0100

    Replace @task for @autotune

--- rosnet/task/util.py
+++ rosnet/task/util.py
@@ -3,22 +3,22 @@ from pycompss.api.parameter import IN, INOUT
 import numpy as np
 
 
-@task(block=IN)
+@autotune(block=IN)
 def getitem(block: np.ndarray, idx):
     return block[idx]
 
 
-@task(block=INOUT)
+@autotune(block=INOUT)
 def setitem(block: np.ndarray, idx, value):
     block[idx] = value
 
 
-@task(block=INOUT, returns=np.ndarray)
+@autotune(block=INOUT, returns=np.ndarray)
 def reshape(block: np.ndarray, shape):
     # WARNING unsafe
     block.shape = shape
 
 
-@task(block=IN, returns=np.ndarray)
+@autotune(block=IN, returns=np.ndarray)
 def copy(block: np.ndarray):
     return np.copy(block)
