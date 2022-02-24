64be612333f55cba0297152c7869f2b7217cd115
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-02-24 09:46:51 +0100

    Remove autoray dispatch from tuning system

--- rosnet/tuning/flops.py
+++ rosnet/tuning/flops.py
@@ -1,19 +1,10 @@
 from math import prod
 import numpy as np
-from plum import dispatch
 from rosnet.helper.macros import implements
 from rosnet.helper.typing import Array
 from rosnet.helper.math import result_shape
 
 
-# @implements("tensordot", ext="tuning.flops")
-# @dispatch.abstract
-# def tensordot():
-#     pass
-
-
-@implements("tensordot", ext="tuning.flops")
-# @dispatch
 def tensordot(a: Array, b: Array, axes) -> int:
     assert all(a.shape[i] == b.shape[j] for i, j in zip(*axes))
 
@@ -22,22 +13,14 @@ def tensordot(a: Array, b: Array, axes) -> int:
     return prod(outer_shape + inner_shape)
 
 
-# @dispatch
-# def tensordot(a: List[Array], b: List[Array], axes) -> int:
-#     pass
-
-
-@implements("full", ext="tuning.flops")
 def full(shape, fill_value, dtype=None, **kwargs) -> int:
     return prod(shape)
 
 
-@implements("reshape", ext="tuning.flops")
 def reshape(a: Array, shape, **kwargs) -> int:
     # TODO does not change/move data, only the .shape attr. maybe if bad alignment afterwards?
     return 1
 
 
-@implements("transpose", ext="tuning.flops")
 def transpose(a: Array, axes=None, **kwargs) -> int:
     return a.size
