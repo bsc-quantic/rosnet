eefa245475ab02e25edf1ca140535ae9948d9eb0
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-01 18:34:18 +0100

    Document interfaces

--- rosnet/core/interface.py
+++ rosnet/core/interface.py
@@ -10,6 +10,10 @@ def hasproperty(cls, name) -> bool:
 
 
 class ArrayConvertable(metaclass=abc.ABCMeta):
+    """Formal interface for classes that can be transformed into numpy arrays.
+    Implements the ArrayLike protocol from numpy.
+    """
+
     @classmethod
     def __subclasshook__(cls, subclass: type):
         return hasmethod(subclass, "__array__")
@@ -20,12 +24,16 @@ for typ in [bool, int, float, complex, str, bytes]:
 
 
 class ArrayDispatchable(metaclass=abc.ABCMeta):
+    """Formal interface for classes that implement the numpy dispatch mechanism."""
+
     @classmethod
     def __subclasshook__(cls, subclass: type):
         return hasmethod(subclass, "__array_ufunc__") and hasmethod(subclass, "__array_function__")
 
 
 class Array(metaclass=abc.ABCMeta):
+    """Formal interface for classes that fulfill the ArrayConvertable and ArrayDispatchable interfaces."""
+
     @classmethod
     def __subclasshook__(cls, subclass: type):
         return issubclass(subclass, ArrayConvertable) and issubclass(subclass, ArrayDispatchable) and hasproperty(subclass, "shape") and hasproperty(subclass, "dtype")
