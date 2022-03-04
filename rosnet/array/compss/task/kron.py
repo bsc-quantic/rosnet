9e4b10b91ec9fbe34fc564b95e24ba16218813c7
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-04 18:52:20 +0100

    Refactor type annotations in "kron" task

--- rosnet/array/compss/task/kron.py
+++ rosnet/array/compss/task/kron.py
@@ -1,7 +1,8 @@
 import numpy as np
 from rosnet.tuning.task import autotune
+from rosnet.core.interface import Array
 
 
 @autotune(returns=np.ndarray)
-def kron(a: np.ndarray, b: np.ndarray):
+def kron(a: Array, b: Array) -> Array:
     return np.kron(a, b)
