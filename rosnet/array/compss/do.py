f47644599ab2efab9317bb09c5c89211188e1aad
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-07 09:35:52 +0100

    Add custom "do" routine for array operation dispatch support for DataClayBlock

---
+++ rosnet/array/compss/do.py
@@ -0,0 +1,29 @@
+import numpy as np
+import autoray
+from itertools import chain
+
+try:
+    from .dataclay import DataClayBlock
+
+    def do(fn_name: str, *args, **kwargs):
+        """Wrapper for 'autoray.do' when DataClayBlocks are involved.
+        In order to execute in dataClay memory space, a DataClayObject must be called from a method. This wrapper does so by detecting if any of the arguments is a DataClayBlock and calling the '__array_ufunc_no_expansion__'  and '__array_function__' methods, with the function returned by 'autoray.get_lib_fn'.
+        """
+        backend = kwargs.pop("like", "numpy")
+        fn = autoray.get_lib_fn(backend, fn_name)
+
+        out = kwargs.get("out", None)
+        if any(isinstance(arg, DataClayBlock) for arg in chain(args, [out])):
+            caller: DataClayBlock = out or next(isinstance(arg, DataClayBlock) for arg in args)
+            if isinstance(fn, np.ufunc):
+                # NOTE "method" arg not supported
+                return caller.__array_ufunc_no_expansion__(fn, "", args, kwargs)
+            else:
+                # NOTE "types" arg not supported
+                return caller.__array_function__(fn, ..., args, kwargs)
+
+        return fn(*args, **kwargs)
+
+
+except ImportError:
+    do = autoray.do
