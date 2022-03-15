65f21341ed39224de7b9437f21b1adbeabea4118
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-15 18:54:50 +0100

    Export svd_vals task

--- rosnet/array/compss/task/__init__.py
+++ rosnet/array/compss/task/__init__.py
@@ -2,7 +2,7 @@ from . import tensordot
 from .init import full, rand, upload
 from .util import reshape, setitem, getitem, copy
 from .transpose import transpose
-from .svd import svd, svd_matrix
+from .svd import svd, svd_matrix, svd_vals
 from .slicing import split, stack
 from .kron import kron
 from .functional import operate, ioperate
