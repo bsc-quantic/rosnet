c304fa2d04065cc24572d50821f7a036f4622b54
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-15 18:54:18 +0100

    Add return type hint to svd tasks

--- rosnet/array/compss/task/svd.py
+++ rosnet/array/compss/task/svd.py
@@ -5,17 +5,17 @@ from rosnet.core.interface import Array
 from rosnet.tuning.task import autotune
 
 
-@autotune(a=IN)
+@autotune(a=IN, returns=Tuple[Array, Array, Array])
 def svd(a: Array, full_matrices=False, hermitian=False) -> Tuple[Array, Array, Array]:
     return np.linalg.svd(a, full_matrices=full_matrices, compute_uv=True, hermitian=hermitian)
 
 
-@autotune(a=IN)
+@autotune(a=IN, returns=Array)
 def svd_vals(a: Array, hermitian=False) -> Array:
     return np.linalg.svd(a, full_matrices=False, compute_uv=False, hermitian=hermitian)
 
 
-@autotune(a=IN)
+@autotune(a=IN, returns=Tuple[Array, Array])
 def svd_matrix(a: Array, full_matrices=False, hermitian=False) -> Tuple[Array, Array]:
     U, s, Vt = np.linalg.svd(a, full_matrices=full_matrices, compute_uv=True, hermitian=hermitian)
     U = U @ np.diag(s)
