3aaebaac900929bbf9d98801ac77d6819fc082a9
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-16 11:29:12 +0100

    Fix multiple return values unpacking in SVD tasks

--- rosnet/array/compss/task/svd.py
+++ rosnet/array/compss/task/svd.py
@@ -5,7 +5,7 @@ from rosnet.core.interface import Array
 from rosnet.tuning.task import autotune
 
 
-@autotune(a=IN, returns=Tuple[Array, Array, Array])
+@autotune(a=IN, returns=3)
 def svd(a: Array, full_matrices=False, hermitian=False) -> Tuple[Array, Array, Array]:
     return np.linalg.svd(a, full_matrices=full_matrices, compute_uv=True, hermitian=hermitian)
 
@@ -15,7 +15,7 @@ def svd_vals(a: Array, hermitian=False) -> Array:
     return np.linalg.svd(a, full_matrices=False, compute_uv=False, hermitian=hermitian)
 
 
-@autotune(a=IN, returns=Tuple[Array, Array])
+@autotune(a=IN, returns=2)
 def svd_matrix(a: Array, full_matrices=False, hermitian=False) -> Tuple[Array, Array]:
     U, s, Vt = np.linalg.svd(a, full_matrices=full_matrices, compute_uv=True, hermitian=hermitian)
     U = U @ np.diag(s)
