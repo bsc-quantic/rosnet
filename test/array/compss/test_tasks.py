0d454f14b92913d5a343744e35f71a953f1e0e95
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-22 18:51:34 +0100

    Init COMPSs tasks tests

---
+++ test/array/compss/test_tasks.py
@@ -0,0 +1,96 @@
+from math import prod
+import pytest
+import numpy as np
+from autoray import do
+
+pycompss = pytest.importorskip("pycompss")
+from rosnet import COMPSsArray
+
+
+@pytest.mark.xfail()
+class TestTensordot:
+    def test_tensordot(self):
+        pass
+
+    def test_sequential(self):
+        pass
+
+    def test_commutative(self):
+        pass
+
+
+class TestTranspose:
+    a = np.random.rand(2, 1, 4, 8)
+
+    @pytest.mark.parametrize(
+        "axes",
+        [
+            # identity
+            list(range(a.ndim)),
+            # singleton dim
+            (1, 0, 2, 3),
+            (0, 2, 1, 3),
+            (0, 3, 2, 1),
+            # reverse
+            (3, 2, 1, 0),
+            # random
+            (2, 1, 3, 0),
+        ],
+    )
+    def test_transpose(self, axes):
+        a = COMPSsArray(self.a)
+        b = np.array(do("transpose", a, axes))
+
+        assert np.array_equal(b, np.transpose(self.a, axes))
+
+
+class TestReshape:
+    a = np.random.rand(2, 1, 4, 8)
+
+    @pytest.mark.parametrize(
+        "shape",
+        [
+            # identity
+            a.shape,
+            # vector
+            (prod(a.shape),),
+            # vector passed by int
+            prod(a.shape),
+            # shrink dims
+            (2, 4, 8),
+            (2, 32),
+            (8, 8),
+            # expand dims
+            (2, 1, 2, 2, 8),
+            (2, 1, 2, 2, 2, 4),
+            (2, 1, 2, 2, 2, 2, 2),
+            # add singleton dims
+            (*a.shape, 1, 1),
+            (1, 1, *a.shape),
+            # -1 inference
+            (-1, *a.shape[0:1]),
+            (*a.shape[0:1], -1),
+            (*a.shape[0:1], -1, 2),
+            # TODO (2, -1, 4, -1, 2), must fail
+        ],
+    )
+    def test_reshape(self, shape):
+        a = COMPSsArray(self.a)
+        b = np.array(do("reshape", a, shape))
+
+        assert np.array_equal(b, np.reshape(self.a, shape))
+
+
+@pytest.mark.xfail()
+class TestSlicing:
+    pass
+
+
+@pytest.mark.xfail()
+class TestSVD:
+    pass
+
+
+@pytest.mark.xfail()
+class TestQR:
+    pass
