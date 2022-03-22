6d55802cea866cf63120a19297a43c684d8c7c62
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-22 19:01:42 +0100

    Add assertion in COMPSsArray.reshape

--- rosnet/array/compss/__init__.py
+++ rosnet/array/compss/__init__.py
@@ -302,6 +302,8 @@ def reshape(a: COMPSsArray, shape, order="C", inplace=False):
         inferred_value = -prod(a.shape) // prod(shape)
         shape = tuple(inferred_value if d == -1 else d for d in shape)
 
+    assert prod(a.shape) == shape
+
     if inplace:
         # TODO support order
         task.reshape_inplace(a.data, shape)
