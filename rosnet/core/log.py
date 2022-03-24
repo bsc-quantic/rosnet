a2a201906988cf58354b92bd0db16538305c6372
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-24 13:10:40 +0100

    Init skeleton for log_args decorator

--- rosnet/core/log.py
+++ rosnet/core/log.py
@@ -1,5 +1,8 @@
+from typing import Callable
+import functools
 import logging
 import sys
+import inspect
 
 logging.getLogger("rosnet").addHandler(logging.NullHandler())
 
@@ -15,3 +18,32 @@ if __debug__:
         handler.setFormatter(formatter)
 
         logger.addHandler(handler)
+
+
+class ArgumentLog:
+    def __init__(self, logger: logging.Logger, level: int = logging.DEBUG):
+        if not isinstance(logger, logging.Logger):
+            raise ValueError(f"logging.Logger must be passed but {type(logger)} instead")
+
+        self.logger = logger
+        self.level = level
+
+    def __call__(self, f: Callable) -> Callable:
+        spec = inspect.getfullargspec(f)
+        if spec.varargs is not None:
+            raise NotImplementedError("varargs not supported in Log decorator yet")
+
+        msg = str.join(", ", ("%s={}" % arg for arg in spec.args))
+
+        @functools.wraps(f)
+        def wrapper(self, *args, **kwargs):
+            try:
+                ret = f(*args, **kwargs)
+            finally:
+                # TODO use Log.level
+                logger.debug(msg.format(*args))
+
+        return wrapper
+
+
+log_args = ArgumentLog
