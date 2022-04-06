8fc07f13a4727808d2827e3c9cd83023003453f5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-04-06 11:51:20 +0200

    Fix pyCOMPSs-cli build on old commits

--- .devcontainer/build-compss.sh
+++ .devcontainer/build-compss.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# commit that introduced --cli flag to buildlocal
+COMPSS_CLI_COMMIT=b23b5c8dda337a09021e34232d49227d1b67c22e
+
 git clone https://github.com/bsc-wdc/compss.git /opt/compss-repo
 cd /opt/compss-repo
 git checkout ${COMPSS_TAG}
@@ -31,7 +34,7 @@ fi
 if [ "$COMPSS_KAFKA" = "true" ]; then \
 	args+=(--kafka)
 fi
-if [ "$COMPSS_CLI" = "true" ]; then \
+if [ "$COMPSS_CLI" = "true" ] && [ $(git merge-base --is-ancestor $COMPSS_CLI_COMMIT $COMPSS_TAG) ]; then \
 	args+=(--cli)
 fi
 if [ "$COMPSS_SKIP_TESTS" = "true" ]; then \
