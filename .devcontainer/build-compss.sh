24779c97fa74fcf315eeda360276ec55ffc87985
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-04-06 11:53:05 +0200

    Fix COMPSs build caching by ARG triggering

--- .devcontainer/build-compss.sh
+++ .devcontainer/build-compss.sh
@@ -3,6 +3,18 @@
 # commit that introduced --cli flag to buildlocal
 COMPSS_CLI_COMMIT=b23b5c8dda337a09021e34232d49227d1b67c22e
 
+COMPSS_TAG=${1}
+COMPSS_BINDING_C=${2}
+COMPSS_BINDING_PYTHON=${3}
+COMPSS_EXTRAE=${4}
+COMPSS_AUTOPARALLEL=${5}
+COMPSS_SKIP_TESTS=${6}
+COMPSS_MONITOR=${7}
+COMPSS_STREAM=${8}
+COMPSS_JACOCO=${9}
+COMPSS_KAFKA=${10}
+COMPSS_CLI=${11}
+
 git clone https://github.com/bsc-wdc/compss.git /opt/compss-repo
 cd /opt/compss-repo
 git checkout ${COMPSS_TAG}
