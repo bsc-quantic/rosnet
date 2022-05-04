34c13426a453ab6e4820c812c477ed687be1c3c3
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-05-04 11:47:45 +0200

    Implement match cases for COMPSs task direction resolution

--- rosnet/executor.py
+++ rosnet/executor.py
@@ -1,13 +1,15 @@
-from concurrent.futures import Executor, Future as PyFuture
+import functools
 import inspect
 import sys
-from typing import TypeVar, Callable, Generic
-import functools
+from concurrent.futures import Executor
+from concurrent.futures import Future as PyFuture
+from typing import Callable, Generic, TypeVar
+
 import numpy as np
-from pycompss.api.api import compss_wait_on, compss_delete_object
-from pycompss.api.task import task
+from pycompss.api.api import compss_delete_object, compss_wait_on
 from pycompss.api.constraint import constraint
-from pycompss.api.parameter import _Param, INOUT
+from pycompss.api.parameter import INOUT, _Param
+from pycompss.api.task import task
 from pycompss.runtime.management.classes import Future as COMPSsFuture
 
 if sys.version_info >= (3, 10):
@@ -53,9 +55,11 @@ def full_qualname(obj) -> list[str]:
     name.reverse()
     return name
 
+# TODO do numpy's built-in methods (impl. in Cython) work?
 def task_directions(fn: Callable, *args, **kwargs) -> dict[str, int | _Param]:
-    path = full_qualname(fn)
-    match path:
+    module_path = full_qualname(fn)
+    qualname = ".".join(module_path)
+    match module_path:
         # object
         case [_, "__getitem__"]:
             return {"returns": 1}
@@ -64,13 +68,22 @@ def task_directions(fn: Callable, *args, **kwargs) -> dict[str, int | _Param]:
             return {"returns": 0}
 
         # numpy
-        case ["numpy", _] if isinstance(fn, np.ufunc):
-            # TODO ufunc cases
-            ...
+        case ["numpy", "copyto"]:
+            # NOTE just in case, not sure if useful
+            return {"returns": 0, "dst": INOUT}
 
-        case ["numpy", _] if "out" in kwargs:
-            # inplace methods: cumsum, einsum, dot, linalg.multi_dot, outer, trace
+        case ["numpy", x, "at"] if isinstance(x, np.ufunc):
+            raise NotImplementedError()
+
+        case ["numpy", x, "reduceat"] if isinstance(x, np.ufunc):
+            raise NotImplementedError()
+
+        # inplace methods: cumsum, einsum, dot, linalg.multi_dot, outer, trace, Universal Functions (ufunc), all, any, around, round_, fix, [nan][cum]prod, [nan][cum]sum, diff, ediff1d, gradient, cross, trapz, amax, nanmax, amin, nanmin, clip, random.Generator.random, random.Generator.permuted, random.Generator.standard_[exponential|gamma|normal], some ma and ma.MaskedArray methods
+        case ["numpy", _] if "out" in kwargs and kwargs["out"] is not None:
             return {"returns": 0, "out": INOUT}
+
+        case ["numpy", "split"]:
+            raise NotImplementedError()
 
         case ["numpy", "linalg", "qr"]:
             mode = kwargs.get("mode", "reduced")
@@ -81,7 +94,7 @@ def task_directions(fn: Callable, *args, **kwargs) -> dict[str, int | _Param]:
                 case "r":
                     return {"returns": 1}
                 case _:
-                    raise ValueError(f"{path}(..., mode='{mode}') is not valid")
+                    raise ValueError(f"{qualname}(..., mode='{mode}') is not valid")
 
         case ["numpy", "linalg", "svd"]:
             compute_uv = kwargs.get("compute_uv", True)
@@ -93,10 +106,8 @@ def task_directions(fn: Callable, *args, **kwargs) -> dict[str, int | _Param]:
         case ["numpy", "linalg", "lstsq"]:
             return {"returns": 4}
 
-        # TODO more methods
-
-        case ["numpy", "split"]:
-            raise NotImplementedError()
+        case ["numpy", "random", _, "shuffle"]:
+            raise NotImplementedError(f"'{qualname}' acts in-place but COMPSs direction cannot be mark correctly as it is a built-in method. Please use 'numpy.random.Generator.permuted(..., out=OUT)' instead.")
 
         case ["numpy", _]:
             return {"returns": 1}
