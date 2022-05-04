cec1f8ee5eebc4b6f3bb001586d9d0f352923695
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-05-04 16:06:16 +0200

    Test `task_directions` on builtin operators

--- test/test_executor.py
+++ test/test_executor.py
@@ -3,21 +3,67 @@ import operator
 
 import numpy as np
 import pytest
-from rosnet.executor import modulepath
+from rosnet.executor import modulepath, task_directions
 
 pycompss = pytest.importorskip("pycompss")
+from pycompss.api.parameter import INOUT
 
-
-ufuncs = [attr for attr in np.__dict__.values() if isinstance(attr, np.ufunc)]
-np_functions = [attr for attr in np.__dict__.values() if inspect.isfunction(attr)]
-np_functions_aliased = [np.show_config, np.set_string_function, np.disp, np.add_newdoc, np.get_array_wrap, np.deprecate, np.deprecate_with_doc, np.get_include, np.who, np.byte_bounds, np.safe_eval, np.recfromtxt, np.recfromcsv]
-np_linalg_functions = [attr for attr in np.linalg.__dict__.values() if inspect.isfunction(attr)]
+builtin_operators = [attr for attr in operator.__dict__.values() if inspect.isbuiltin(attr)]
+builtin_operators_inplace = [
+    operator.iadd,
+    operator.iand,
+    operator.iconcat,
+    operator.ifloordiv,
+    operator.ilshift,
+    operator.imod,
+    operator.imul,
+    operator.imatmul,
+    operator.ior,
+    operator.ipow,
+    operator.irshift,
+    operator.isub,
+    operator.itruediv,
+    operator.ixor,
+]
 
 
 class TestModulePathBuiltins:
     @pytest.mark.parametrize("builtin", filter(inspect.isbuiltin, operator.__dict__.values()))
     def test_operators(self, builtin):
         assert modulepath(builtin) == ["builtins", builtin.__name__]
+
+
+class TestTaskDirectionsBuiltins:
+    def test_setitem(self):
+        assert task_directions(operator.setitem) == {"returns": 0, "a": INOUT}
+
+    @pytest.mark.parametrize("builtin", filter(lambda x: x not in builtin_operators_inplace and x is not operator.setitem, builtin_operators))
+    def test_builtins(self, builtin):
+        assert task_directions(builtin) == {"returns": 1}
+
+    @pytest.mark.parametrize("builtin", builtin_operators_inplace)
+    def test_builtins_inplace(self, builtin):
+        assert task_directions(builtin) == {"returns": 1, "a": INOUT}
+
+
+ufuncs = [attr for attr in np.__dict__.values() if isinstance(attr, np.ufunc)]
+np_functions = [attr for attr in np.__dict__.values() if inspect.isfunction(attr)]
+np_functions_aliased = [
+    np.show_config,
+    np.set_string_function,
+    np.disp,
+    np.add_newdoc,
+    np.get_array_wrap,
+    np.deprecate,
+    np.deprecate_with_doc,
+    np.get_include,
+    np.who,
+    np.byte_bounds,
+    np.safe_eval,
+    np.recfromtxt,
+    np.recfromcsv,
+]
+np_linalg_functions = [attr for attr in np.linalg.__dict__.values() if inspect.isfunction(attr)]
 
 
 class TestModulePathNumpy:
